9d7f9b8aa5cd6ffba6fc88de8d64c6f96aa46f11
Seeker <baisa.156269@stamaria.sti.edu.ph>
2023-08-12 22:37:08 +0800

    update message

--- src/components/Messaging/Message.jsx
+++ src/components/Messaging/Message.jsx
@@ -210,8 +210,8 @@ const Message = ({ beneemail }) => {
               {studinfo && (
                 <div className="h-[93%] rounded-bl-md overflow-y-auto scroll-smooth">
                   {studinfo 
-                    .sort((a, b) => (a.last_Modif > b.last_Modif ? 1 : -1))
-                    .sort((a, b) => (a.last_Modif > b.last_Modif ? 1 : -1))
+                    .sort((a, b) => (a.last_Modif > b.last_Modif ? -1 : 1))
+                    .sort((a, b) => (a.last_Modif > b.last_Modif ? -1 :  1))
                     .map((studinfo) => (
                       <MessagingConfig
                         key={studinfo.id}
