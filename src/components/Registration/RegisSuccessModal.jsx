51813f21c5ddc7d084f9881560c58ac1284bed15
Seeker <baisa.156269@stamaria.sti.edu.ph>
2023-08-24 23:43:00 +0800

    Regis sucess promt UI adjustment

--- src/components/Registration/RegisSuccessModal.jsx
+++ src/components/Registration/RegisSuccessModal.jsx
@@ -8,12 +8,12 @@ export default function RegisSuccessModal({ visible, onClose }) {
   }
   return (
     <div className="fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center">
-      <div className="bg-white h-[20%] w-[30%] rounded-md text-black" data-aos="zoom-in" data-aos-duration="1000">
+      <div className="bg-white h-[100px] w-[200px] rounded-md text-black" data-aos="zoom-in" data-aos-duration="1000">
         <p className="text-center mt-[10%]">Registry is complete!! </p>
         <div className="flex justify-center mt-[4%]">
           <button
             onClick={refersh}
-            className="bg-blue-400 hover:bg-blue-200 font-semibold w-[20%] h-[10] rounded-md"
+            className="bg-blue-400 hover:bg-blue-200 font-semibold w-[100px] h-[10] rounded-md"
           >
             CLOSE
           </button>
