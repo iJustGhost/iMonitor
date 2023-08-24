92109149fc8a943ae9f3112aa1d7ebe73c057105
Seeker <baisa.156269@stamaria.sti.edu.ph>
2023-08-24 20:52:25 +0800

    sizing announcement module for student

--- src/components/StudentPages/AnnouncementStudent.jsx
+++ src/components/StudentPages/AnnouncementStudent.jsx
@@ -132,7 +132,7 @@ function AnnouncementStudent({ studemail }) {
         <div className="font-bold text-[30px] text-center h-[10%] text-white">
           Announcements
         </div>
-        <div className="bg-gray-400 w-[90%] md:h-[75%] h-[60%]  gap-1 flex rounded-md">
+        <div className="bg-gray-400 w-[90%] md:h-[75%] h-[58%]  gap-1 flex rounded-md">
           <div className="bg-white  md:w-[25%] w-[30%] h-[100%]  rounded-md">
             <div className="overflow-y-auto  rounded-md">
               {announcementinfoState ? (
