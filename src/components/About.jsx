4e5fa16506c90602ce15ab3bbaea8c1d2d21d01e
Seeker <baisa.156269@stamaria.sti.edu.ph>
2023-08-25 18:14:59 +0800

    Component About Deletion

--- src/components/About.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function About() {
-  return (
-    <div>asdfaf Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam tenetur, voluptate repellendus, eligendi atque est debitis dignissimos culpa cumque, aliquid rerum. Corrupti quidem quibusdam magni odio, labore qui neque, doloribus possimus facilis culpa beatae distinctio sit at. Quos commodi ut nisi fugit quisquam ducimus, itaque doloribus quam quae nam? Cupiditate itaque, facilis vel impedit odio deserunt? Alias, error libero saepe, delectus nisi praesentium amet assumenda voluptas vel magni velit veritatis voluptatem sequi nihil repudiandae. Cumque tempora suscipit beatae maxime esse quis? Incidunt veniam dicta optio commodi, ipsum dolores quia nobis. Veritatis optio quidem sequi, sapiente accusantium nobis neque aut iste.About</div>
-  )
-}
-
-export default About
