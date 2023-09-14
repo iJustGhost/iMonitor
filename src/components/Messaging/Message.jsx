6d804d732f3756d059c40bd2ef8b640bbc6e04d8
Seeker <baisa.156269@stamaria.sti.edu.ph>
2023-09-14 12:53:59 +0800

    ui upadte in search bar in messaging

--- src/components/Messaging/Message.jsx
+++ src/components/Messaging/Message.jsx
@@ -262,13 +262,23 @@ const Message = ({ beneemail }) => {
             </p>
             {studinfo ? (
               <div className="h-[93%] rounded-bl-md overflow-y-auto scroll-smooth">
-                <input
-                  value={search}
-                  onChange={(e) => setSearch(e.target.value)}
-                  type="text"
-                  className="w-[100%] bg-slate-200 p-1 border-0 outline-none"
-                  placeholder="Search Here"
-                ></input>
+                <div className="bg-slate-200 flex">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 512 512"
+                    className="h-[13px] w-10 mt-2.5 ml-2"
+                  >
+                    <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
+                  </svg>
+                  <input
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                    type="text"
+                    className="w-[100%] bg-slate-200 p-1 border-0 outline-none"
+                    placeholder="Search Here"
+                  ></input>
+                </div>
+
                 {studinfo
                   .filter((val) => {
                     try {
