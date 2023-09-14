18279277e91cc344643d86020638af5f021cd829
Seeker <baisa.156269@stamaria.sti.edu.ph>
2023-09-14 18:26:55 +0800

    ui stud mess debugged

--- src/components/StudentPages/MessageStudent.jsx
+++ src/components/StudentPages/MessageStudent.jsx
@@ -241,27 +241,27 @@ const MessageStudent = ({ studemail }) => {
               <IoMdContacts className="text-[25px] text-white mr-0.5  mt-1" />
               Contacts
             </p>
+            <center>
+              <div className="bg-slate-300 flex w-[96%] rounded-md place-content-center mt-2 mb-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                  className="h-[13px] w-10 mt-2.5 ml-2"
+                >
+                  <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
+                </svg>
+                <input
+                  value={search}
+                  onChange={(e) => setSearch(e.target.value)}
+                  type="text"
+                  className="w-[100%] bg-slate-300 p-1 border-0 outline-none rounded-md"
+                  placeholder="Search Here"
+                ></input>
+              </div>
+            </center>
 
             {beneinfo && (
               <div className="h-[80%] rounded-bl-md overflow-y-auto scroll-smooth">
-                <center>
-                  <div className="bg-slate-300 flex w-[96%] rounded-md place-content-center mt-2 mb-2">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 512 512"
-                      className="h-[13px] w-10 mt-2.5 ml-2"
-                    >
-                      <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
-                    </svg>
-                    <input
-                      value={search}
-                      onChange={(e) => setSearch(e.target.value)}
-                      type="text"
-                      className="w-[100%] bg-slate-300 p-1 border-0 outline-none rounded-md"
-                      placeholder="Search Here"
-                    ></input>
-                  </div>
-                </center>
                 {beneinfo
                   .filter((val) => {
                     try {
