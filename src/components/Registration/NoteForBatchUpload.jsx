a274aeabb3f964f7eb726bd834ebd7a9b9ac264f
Seeker <baisa.156269@stamaria.sti.edu.ph>
2023-10-11 23:29:44 +0800

    ui note for batchupload

--- src/components/Registration/NoteForBatchUpload.jsx
+++ src/components/Registration/NoteForBatchUpload.jsx
@@ -5,8 +5,8 @@ function NoteForBatchUpload({ visible, close }) {
   if (!visible) return null;
   return (
     <div className="fixed inset-0  flex justify-center items-center">
-      <div className="bg-black bg-opacity-5 backdrop-blur-[1px] h-[600px] w-[800px] flex justify-center items-center">
-        <div className="bg-white h-[40%] w-[65%] shadow-lg shadow-black rounded-md flex flex-col place-content-center items-center">
+      <div className="bg-black bg-opacity-5 backdrop-blur-[1px] h-[650px] w-[800px] flex justify-center items-center">
+        <div className="bg-white h-[35%] w-[60%] shadow-lg shadow-black rounded-md flex flex-col place-content-center items-center">
           <div className="text-center flex-col justify-center items-center flex pb-[20px]">
             <RiInformationFill className="text-blue-500 text-[50px]  rounded-full   " />
             <label className="w-[500px]  ">
